6129cac5ac8a1c4d0cbec03891b56839331d8c17
IKEP <34697515+creativeIKEP@users.noreply.github.com>
2021-05-17 22:30:19 +0900

    prepare development enviroment

---
+++ Assets/Script/WebCamInput.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamInput : MonoBehaviour
+{
+    [SerializeField] string webCamName;
+    [SerializeField] Vector2 webCamResolution;
+    [SerializeField] RawImage rawImage;
+    [SerializeField] Texture staticInput;
+
+    // Provide input image Texture.
+    public Texture inputImageTexture{
+        get{
+            if(staticInput != null) return staticInput;
+            return inputRT;
+        }
+    }
+
+    WebCamTexture webCamTexture;
+    RenderTexture inputRT;
+
+    void Start()
+    {
+        if(staticInput == null){
+            webCamTexture = new WebCamTexture(webCamName, (int)webCamResolution.x, (int)webCamResolution.y);
+            webCamTexture.Play();
+        }
+
+        inputRT = new RenderTexture((int)webCamResolution.x, (int)webCamResolution.y, 0);
+        rawImage.texture = inputImageTexture;
+    }
+
+    void Update()
+    {
+        if(staticInput != null) return;
+
+        var aspect1 = (float)webCamTexture.width / webCamTexture.height;
+        var aspect2 = (float)inputRT.width / inputRT.height;
+        var aspectGap = aspect2 / aspect1;
+
+        var vMirrored = webCamTexture.videoVerticallyMirrored;
+        var scale = new Vector2(aspectGap, vMirrored ? -1 : 1);
+        var offset = new Vector2((1 - aspectGap) / 2, vMirrored ? 1 : 0);
+
+        Graphics.Blit(webCamTexture, inputRT, scale, offset);
+    }
+
+    void OnDestroy(){
+        if (webCamTexture != null) Destroy(webCamTexture);
+        if (inputRT != null) Destroy(inputRT);
+    }
+}
---
+++ Packages/BlazePoseBarracuda/Script/BlazePoseDetecter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mediapipe.PoseDetection;
+using Mediapipe.PoseLandmark;
+
+namespace Mediapipe.BlazePose{
+    public class BlazePoseDetecter
+    {
+
+    }
+}
